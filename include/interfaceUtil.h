2b5c421231ae3488750ef404da45df884eb6ecd2
Valdemar Linus <Valdemar Kaasgaard>
2025-11-21 10:07:19 +0100

    2

---
+++ include/interfaceUtil.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <iostream>
+#include "imgui_internal.h"
+
+
+namespace windowVar {
+    extern int monitor_w;
+    extern int monitor_h;
+    extern int display_w;
+    extern int display_h;
+    extern ImVec4 BackgroundColor;
+}
+
+class WindowInitializer {
+public:
+    
+    void GetPrimaryMonitorResolution(int& width, int& height);
+    void Setup();
+    void UpdateWindowSize();
+    
+    void Render();
+    void DrawMultiColor();
+    void loadFonts();     // load all fonts once
+    ImFont* getFont(int size);
+
+private:
+    ImFont* font18 = nullptr;
+    ImFont* font24 = nullptr;
+    ImFont* font28 = nullptr;
+    ImFont* font40 = nullptr;
+};
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+ImU32 DarkenColor(ImU32 col, float factor);
+class Widgets {
+public:
+    bool costum_square_button(const char* id, ImVec2 pos, ImVec2 size, ImFont* font, float font_size, ImU32 color);
+    bool costum_round_button(ImVec2 center, float radius, int segments, ImU32 color);
+    bool DrawCircleGradientButton(ImDrawList* draw_list, ImFont* font, float scale, ImVec2 center, float radius, const char* id, float font_size);
+};
+
+bool LoadTextureFromMemory(const void* data, size_t data_size, GLuint* out_texture, int* out_width, int* out_height);
+bool LoadTextureFromFile(const char* file_name, GLuint* out_texture, int* out_width, int* out_height);
---
+++ src/interface.cpp
@@ -0,0 +1,145 @@
+#include "interfaceUtil.h"
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <filesystem>
+#include "stb_image.h"
+using namespace std;
+
+
+WindowInitializer winInit;
+Widgets widget;
+
+//setup
+int current_image = 1;
+int max_images = 10;
+int button_width = 125;
+
+
+int main() {
+    // Load image
+    
+
+    //std::string base = std::filesystem::current_path().string();
+    //std::cout << "Working directory: " << base << "\n";
+
+
+    if (!glfwInit())
+        return 1;
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    
+    
+
+    // Find screen resolution
+    winInit.GetPrimaryMonitorResolution(windowVar::monitor_w, windowVar::monitor_h);
+    windowVar::display_w = windowVar::monitor_w;
+    windowVar::display_h = windowVar::monitor_h;
+    
+
+    
+
+    winInit.Setup();
+
+    GLFWwindow* window = glfwCreateWindow(windowVar::display_w, windowVar::display_h, "UI or something", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+
+
+   
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); 
+
+    int my_image_width = 0;
+    int my_image_height = 0;
+    GLuint my_image_texture = 0;
+    std::string path = "images/jesper" + current_image + ".jpeg";   
+    std::cout << path << std::endl;
+    bool ret = LoadTextureFromFile("/home/valdemar-linus/DroneBoys/images/jesper1.jpeg", &my_image_texture, &my_image_width, &my_image_height);
+    IM_ASSERT(ret);
+
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        int fb_w, fb_h;
+        glfwGetFramebufferSize(window, &fb_w, &fb_h);
+        
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();     
+        
+        //Page Control Button
+        ImGui::NewFrame();
+        ImGui::SetNextWindowSize(ImVec2(button_width ,200));
+        ImGui::SetNextWindowPos(ImVec2(5, 5));
+        ImGui::Begin("Image Control");
+        if (ImGui::Button("next image")) {;
+            if (current_image <  max_images)
+                current_image++;
+
+        }
+        if (ImGui::Button("previous image"))  {;
+            if (current_image > 1)
+                current_image--;
+
+        }
+         if (ImGui::Button("method 1")) {;
+            printf("wow this sure is a temporary print fuction");
+
+        }
+        if (ImGui::Button("method 2"))  {;
+            printf("wow this sure is a temporary print fuction");
+
+
+        }
+
+        ImGui::End();
+
+        //Image Window
+        ImGui::SetNextWindowSize(ImVec2((float)fb_w * 0.98 -button_width, (float)fb_h * 0.7));
+        ImGui::SetNextWindowPos(ImVec2(button_width+15, 5));
+        ImGui::Begin("Image Goes Here");
+        ImGui::Image((ImTextureID)(intptr_t)my_image_texture, ImVec2((float)fb_w * 0.96 -button_width, (float)fb_h * 0.68));
+        ImGui::End();
+        
+
+        //description window
+        ImGui::SetNextWindowSize(ImVec2((float)fb_w * 0.98 -button_width, (float)fb_h * 0.3 - 15));
+        ImGui::SetNextWindowPos(ImVec2(button_width+15, (float)fb_h * 0.7+10));
+        ImGui::Begin("Description");
+        ImGui::Text("This is image %d", current_image);
+        ImGui::End();
+
+
+        ImGui::Render();
+        glViewport(0, 0, fb_w, fb_h);
+        glClearColor(windowVar::BackgroundColor.x, windowVar::BackgroundColor.y, windowVar::BackgroundColor.z, windowVar::BackgroundColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+   
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+
+        glfwSwapBuffers(window);
+        
+    }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();    
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+
+
+
+
+    return 0;   
+} 
